d91939370c0753c1bc56cea89a21c45787400667
roybhaskar9 <bhaskar@study9.com>
2018-03-23 22:04:49 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@ sudo apt-get install -y puppet
 sudo puppet module install rtyler-jenkins
 sudo puppet module install hubspot-nexus
 sudo puppet module install maestrodev-sonarqube
+sudo puppet apply jenkins.pp
+sudo puppet apply nexus.pp
+sudo puppet apply sonar.pp
+/etc/init.d/sonar start
