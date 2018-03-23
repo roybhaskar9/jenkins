22c1f28eec204a26e544e21e0caff1f9b296047c
roybhaskar9 <bhaskar@study9.com>
2018-03-23 22:13:20 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,12 @@ sudo apt-get install -y puppet
 sudo puppet module install rtyler-jenkins
 sudo puppet module install hubspot-nexus
 sudo puppet module install maestrodev-sonarqube
+wget https://raw.githubusercontent.com/roybhaskar9/jenkins/master/jenkins.pp
+wget https://raw.githubusercontent.com/roybhaskar9/jenkins/master/nexus.pp
+wget https://raw.githubusercontent.com/roybhaskar9/jenkins/master/sonar.pp
 sudo puppet apply jenkins.pp
 sudo puppet apply nexus.pp
 sudo puppet apply sonar.pp
 /etc/init.d/sonar start
+rm -f /etc/maven/settings.xml
+wget https://raw.githubusercontent.com/roybhaskar9/samplejava/master/settings.xml -O /etc/maven/settings.xml
