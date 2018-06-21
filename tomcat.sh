57594e66f6bbd5a9d76235aed9fe1353e1ec92e0
roybhaskar9 <bhaskar@study9.com>
2018-06-21 08:54:30 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
-sudo apt-get install tomcat7 tomcat7-admin
+sudo apt-get -y install tomcat7 tomcat7-admin lynx
 wget https://raw.githubusercontent.com/roybhaskar9/samplejava/master/tomcat-users.xml -O /etc/tomcat7/tomcat-users.xml
 sudo service tomcat7 restart
+lynx localhost:8080
