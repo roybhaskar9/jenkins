9bb3487b052a7f14c6383280dc9c9b1ca0948d6f
roybhaskar9 <bhaskar@study9.com>
2018-06-22 09:07:16 +0530

    Update and rename tomcat.sh to tomcatubuntu.sh

--- tomcatubuntu.sh
+++ tomcatubuntu.sh
@@ -2,4 +2,3 @@ sudo apt-get update
 sudo apt-get -y install tomcat7 tomcat7-admin lynx
 wget https://raw.githubusercontent.com/roybhaskar9/samplejava/master/tomcat-users.xml -O /etc/tomcat7/tomcat-users.xml
 sudo service tomcat7 restart
-lynx localhost:8080
